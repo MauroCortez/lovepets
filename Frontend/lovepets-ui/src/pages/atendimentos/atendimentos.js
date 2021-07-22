b402dd8d4195f075480b6dc60b0b1046f44bff2d
Mauro Bastos Cortez <45763910893@info.local>
2021-07-22 17:20:41 -0300

    React atualizado

--- Frontend/lovepets-ui/src/pages/atendimentos/atendimentos.js
+++ Frontend/lovepets-ui/src/pages/atendimentos/atendimentos.js
@@ -1,11 +1,21 @@
 import { Component } from "react";
+import axios from "axios";
 
 export default class Atendimentos extends Component{
   constructor(props){
     super(props);
     this.state = {
       // nomeEstado : valorInicial
-      listaAtendimentos : []
+      listaAtendimentos : [],
+      idPet : 0,
+      idVeterinario : 0,
+      descricao : '',
+      data : new Date(),
+      hora : '',
+      idSituacao : 0,
+      listaVeterinarios : [],
+      listaPets : [],
+      listaSituacoes : []
     };
   };
 
@@ -31,8 +41,104 @@ export default class Atendimentos extends Component{
     .catch(erro => console.log(erro));
   };
 
+  buscarVeterinarios = () => {
+    fetch('http://localhost:5000/api/veterinarios', {
+      headers : {
+        'Authorization' : 'Bearer ' + localStorage.getItem('usuario-login')
+      }
+    })
+
+    .then(resposta => {
+      if (resposta.status !== 200) {
+        throw Error();
+      }
+
+      return resposta.json();
+    })
+
+    .then(resposta => this.setState({ listaVeterinarios : resposta }))
+
+    .catch(erro => console.log(erro));
+  };
+
+  buscarPets = () => {
+    fetch('http://localhost:5000/api/pets', {
+      headers : {
+        'Authorization' : 'Bearer ' + localStorage.getItem('usuario-login')
+      }
+    })
+
+    .then(resposta => {
+      if (resposta.status !== 200) {
+        throw Error();
+      }
+
+      return resposta.json();
+    })
+
+    .then(resposta => this.setState({ listaPets : resposta }))
+
+    .catch(erro => console.log(erro));
+  };
+
+  buscarSituacoes = () => {
+    fetch('http://localhost:5000/api/situacaos', {
+      headers : {
+        'Authorization' : 'Bearer ' + localStorage.getItem('usuario-login')
+      }
+    })
+
+    .then(resposta => {
+      if (resposta.status !== 200) {
+        throw Error();
+      }
+
+      return resposta.json();
+    })
+
+    .then(resposta => this.setState({ listaSituacoes : resposta }))
+
+    .catch(erro => console.log(erro));
+  };
+
   componentDidMount(){
     this.buscarAtendimentos();
+    this.buscarVeterinarios();
+    this.buscarPets();
+    this.buscarSituacoes();
+  };
+
+  cadastrarAtendimento = (event) => {
+    event.preventDefault();
+
+    let atendimento = {
+      idPet                     :     this.state.idPet,
+      idVeterinario             :     this.state.idVeterinario,
+      descricao                 :     this.state.descricao,
+      dataAtendimento           :     this.state.data + 'T' + this.state.hora,
+      idSituacao                :     this.state.idSituacao
+    };
+
+    axios.post('http://localhost:5000/api/atendimentos', atendimento, {
+      headers : {
+        'Authorization' : 'Bearer ' + localStorage.getItem('usuario-login')
+      }
+    })
+
+    .then(resposta => {
+      if (resposta.status === 201) {
+        console.log('Um novo atendimento foi agendado!')
+      }
+    })
+
+    .catch(erro => console.log(erro))
+
+    .then(this.buscarAtendimentos);
+  }
+
+  atualizaStateCampo = (campo) => {
+    // exemplo          idPet           :       1
+    this.setState({ [campo.target.name] : campo.target.value })
   };
 
   render(){
@@ -86,6 +192,125 @@ export default class Atendimentos extends Component{
 
         <section>
           <h2>Cadastro de Atendimentos</h2>
+
+          {/* Formulário de cadastro */}
+
+          <form onSubmit={this.cadastrarAtendimento}>
+
+          <select
+            name="idPet"
+            value={this.state.idPet}
+            onChange={this.atualizaStateCampo}
+          >
+            <option value="0">Selecione o pet que será atendido</option>
+
+            {
+              this.state.listaPets.map( (pet) => {
+                return(
+                  <option key={pet.idPet} value={pet.idPet}>{pet.nomePet}</option>
+                )
+              } )
+            }
+
+
+          </select>
+
+            {/* <input 
+              // Pet
+              type="number"
+              name="idPet"
+              value={this.state.idPet}
+              onChange={this.atualizaStateCampo}
+            /> */}
+
+            <select
+              name="idVeterinario"
+              value={this.state.idVeterinario}
+              onChange={this.atualizaStateCampo}
+            >
+
+              <option value="0">Selecione o veterinário que fará o atendimento</option>
+
+              {
+                this.state.listaVeterinarios.map( (veterinario) => {
+                  return(
+                    <option key={veterinario.idVeterinario} value={veterinario.idVeterinario}>{veterinario.nomeVeterinario} - {veterinario.crmv}</option>
+                  )
+                } )
+              }
+
+            </select>
+
+            {/* 
+
+            Outra forma
+            
+            <input 
+              // Veterinário
+              type="number"
+              name="idVeterinario"
+              value={this.state.idVeterinario}
+              onChange={this.atualizaStateCampo}
+            /> 
+            
+            */}
+
+            <input 
+              // Descrição
+              type="text"
+              name="descricao"
+              value={this.state.descricao}
+              onChange={this.atualizaStateCampo}
+            />
+
+            <input 
+              // Data do atendimento
+              type="date"
+              name="data"
+              value={this.state.data}
+              onChange={this.atualizaStateCampo}
+            />
+
+            <input 
+              // Hora do atendimento
+              type="time"
+              name="hora"
+              value={this.state.hora}
+              onChange={this.atualizaStateCampo}
+            />
+
+            <select 
+              name="idSituacao"
+              value={this.state.idSituacao}
+              onChange={this.atualizaStateCampo}
+            >
+              <option value="0">Selecione a Situação</option>
+
+              {
+                  this.state.listaSituacoes.map( (situacao) => {
+                    return(
+                      <option key={situacao.idSituacao} value={situacao.idSituacao}>{situacao.nomeSituacao}</option>
+                    )
+                  } )
+              }
+
+
+            </select>
+
+            {/* <input 
+              // Situação
+              type="number"
+              name="idSituacao"
+              value={this.state.idSituacao}
+              onChange={this.atualizaStateCampo}
+            /> */}
+
+            <button type="submit">
+              Cadastrar
+            </button>
+
+          </form>
+
         </section>
       </div>
     )
